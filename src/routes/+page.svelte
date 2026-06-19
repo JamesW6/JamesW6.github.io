44f4ed427d72a650313b1862e8ff976e62aa337f
James Widmer <jow257@pitt.edu>
2026-06-19 12:27:26 -0400

    added coloring, content, and dynamic sizing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<div id="webpage"  class="py-10 grid grid-cols-1 gap-10 wrap justify-items-center bg-rose-100 font-montserrat">
+  <div id = "content" class="grid grid-cols-1 justify-items-start gap-10 max-inline-150 p-3">
+    <div id="nav" class="justify-items-center">
+      <a href="#about">About</a>
+      <a href="#projects">Projects</a>
+      <a href="https://drive.google.com/file/d/1N85SLwVIxC-WtLHNy610Iddi7XmVOO1P/view?usp=sharing" target="_blank" class="p-1 bg-black text-white">Resume</a>
+    </div>
+
+    <div id="highlights" class="">
+      <h1 class="inline-5 text-2xl font-bold">James Widmer</h1>
+      <p>Sophomore @ Pitt</p>
+      <p>Incoming CS0441 UTA @ Pitt</p>
+    </div>
+
+    <div id="highlights">
+      <p>
+        Hi, I'm James, a rising sophomore at Pitt studying computer science with a minor in mathematics. I've previously worked as web designer with the 
+        Osher Lifelong Learning Institute at Pitt and participated in an internship-like environment with the Computer Science Club's Devlab last semester,
+        building the Pitt Meals Project. I'm currently looking for Sumer 2027 internship positions, feel free to reach out and connect! Outside of tech I'm into 
+        rock climbing and watching movies, but I'm down for pretty much anything.
+      </p>
+    </div>
+    <div id="projects">
+      <p>All my projects live on Github, here are a few of my favorites. More to Come.</p>
+      <a href="https://github.com/jamesw6" target="_blank">github.com/jamesw6</a>
+    </div>
+  </div>
+</div>
+
