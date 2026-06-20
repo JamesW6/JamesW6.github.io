2c395f0d9366d3dd7e60a021c0d415c103639a83
James Widmer <jow257@pitt.edu>
2026-06-20 11:13:17 -0400

    adds my github and some notable repositories are highlighted

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,5 @@
-<div id="webpage"  class="py-10 grid grid-cols-1 gap-10 wrap justify-items-center bg-rose-100 font-montserrat">
-  <div id = "content" class="grid grid-cols-1 justify-items-start gap-10 max-inline-150 p-3">
+<div id="webpage"  class="py-10 grid grid-cols-1 gap-12 wrap justify-items-center bg-rose-100 font-montserrat">
+  <div id = "content" class="grid grid-cols-1 justify-items-start gap-12 max-inline-150 p-3">
     <div id="nav" class="justify-items-center">
       <a href="#about">About</a>
       <a href="#projects">Projects</a>
@@ -12,17 +12,39 @@
       <p>Incoming CS0441 UTA @ Pitt</p>
     </div>
 
+
+    <div class="border border-black inline-full"></div>
+
     <div id="highlights">
       <p>
-        Hi, I'm James, a rising sophomore at Pitt studying computer science with a minor in mathematics. I've previously worked as web designer with the 
+        Hi, I'm James, a rising sophomore at Pitt studying computer science with a minor in mathematics and a vim and linux hobbyist. I've previously worked as web designer with the 
         Osher Lifelong Learning Institute at Pitt and participated in an internship-like environment with the Computer Science Club's Devlab last semester,
-        building the Pitt Meals Project. I'm currently looking for Sumer 2027 internship positions, feel free to reach out and connect! Outside of tech I'm into 
+        building the Pitt Meals Project. I'm currently looking for internship positions starting in Summer 2027, feel free to reach out and connect! Outside of tech I'm into 
         rock climbing and watching movies, but I'm down for pretty much anything.
       </p>
     </div>
-    <div id="projects">
-      <p>All my projects live on Github, here are a few of my favorites. More to Come.</p>
-      <a href="https://github.com/jamesw6" target="_blank">github.com/jamesw6</a>
+
+    <div class="border border-black inline-full"></div>
+
+    <div class="" id="projects">
+      <div class="grid grid-cols-1 gap-4">
+        <p class="">All my projects live on Github, and i've shared a few of my favorites. More to Come.</p>
+        <a class = "justify-self-center px-2 py-4 my-1 inline-3/4 text-center border border-black hover:border-white hover:text-white hover:bg-black" href="https://github.com/jamesw6" target="_blank">github.com/jamesw6</a>
+      </div>
+      <div class="grid grid-cols-2 py-6">
+        <a href="https://github.com/NatnaelTegegne/piky-park" target="blank">
+          <div class="border-2 border-black overflow-clip hover:bg-rose-300" style="border-right:0">
+              <p class="text-lg">PikiPark</p>
+              <p>ML model for finding free parking spaces</p>
+          </div>
+        </a>
+        <a href="https://github.com/JamesW6/JamesW6.github.io" target="_blank">
+          <div class="border-2 border-black hover:bg-rose-300">
+            <p class="text-lg">This Website</p>
+            <p>This site is how I dived into web development</p>
+          </div>
+        </a>
+      </div>
     </div>
   </div>
 </div>
