4a1ed3ca20837561b34a476d256ebc4184c1d566
James Widmer <jow257@pitt.edu>
2026-06-22 21:06:39 -0400

    fixes some formatting, adds contact methods, adds some highlighting

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
 <div id="webpage"  class="py-10 grid grid-cols-1 gap-12 wrap justify-items-center bg-rose-100 font-montserrat">
-  <div id = "content" class="grid grid-cols-1 justify-items-start gap-12 max-inline-150 p-3">
-    <div id="nav" class="justify-items-center">
-      <a href="#about">About</a>
-      <a href="#projects">Projects</a>
-      <a href="https://drive.google.com/file/d/1N85SLwVIxC-WtLHNy610Iddi7XmVOO1P/view?usp=sharing" target="_blank" class="p-1 bg-black text-white">Resume</a>
+  <div id = "content" class="grid grid-cols-1 justify-items-start gap-12 lg:max-inline-1/3 p-3 min-h-screen">
+    <div id="nav" class="flex items-center gap-2 inline-full">
+      <a href="#about" class="hover:underline">About</a>
+      <a href="#projects" class="hover:underline">Projects</a>
+      <a href="#contact" class="hover:underline">Contact</a>
+      <a href="https://drive.google.com/file/d/1N85SLwVIxC-WtLHNy610Iddi7XmVOO1P/view?usp=sharing" target="_blank" class="p-1 bg-black text-white hover:bg-rose-300 hover:text-black">Resume</a>
+      <p class="text-xs grow text-right invisible md:visible">jow257 [at] pitt [dot] edu</p>
     </div>
 
     <div id="highlights" class="">
@@ -13,39 +15,60 @@
     </div>
 
 
-    <div class="border border-black inline-full"></div>
+    <div class="border border-black inline-full max-h-px"></div>
 
     <div id="highlights">
       <p>
-        Hi, I'm James, a rising sophomore at Pitt studying computer science with a minor in mathematics and a vim and linux hobbyist. I've previously worked as web designer with the 
-        Osher Lifelong Learning Institute at Pitt and participated in an internship-like environment with the Computer Science Club's Devlab last semester,
-        building the Pitt Meals Project. I'm currently looking for internship positions starting in Summer 2027, feel free to reach out and connect! Outside of tech I'm into 
+        Hi, I'm James, a rising sophomore at Pitt studying <b>computer science</b> with a minor in mathematics and a vim and linux hobbyist. I've previously worked as
+        web designer with the Osher Lifelong Learning Institute at Pitt and participated in an internship-like environment with the 
+        Computer Science Club's Devlab last semester,building the Pitt Meals Project. I'm currently looking for
+        <mark class="bg-black text-white text-md">internship</mark> positions starting in 
+        <mark class="bg-black text-white text-md">Summer 2027</mark>, feel free to reach out and connect! Outside of tech I'm into
         rock climbing and watching movies, but I'm down for pretty much anything.
       </p>
     </div>
 
-    <div class="border border-black inline-full"></div>
+    <div class="border border-black inline-full max-h-px"></div>
 
-    <div class="" id="projects">
+    <div class="inline-full" id="projects">
       <div class="grid grid-cols-1 gap-4">
+        <h1 class="text-2xl">Projects</h1>
         <p class="">All my projects live on Github, and i've shared a few of my favorites. More to Come.</p>
         <a class = "justify-self-center px-2 py-4 my-1 inline-3/4 text-center border border-black hover:border-white hover:text-white hover:bg-black" href="https://github.com/jamesw6" target="_blank">github.com/jamesw6</a>
       </div>
-      <div class="grid grid-cols-2 py-6">
-        <a href="https://github.com/NatnaelTegegne/piky-park" target="blank">
-          <div class="border-2 border-black overflow-clip hover:bg-rose-300" style="border-right:0">
+      <div class="grid grid-cols-2 grid-rows-1 auto-rows-max py-6">
+        <a href="https://github.com/NatnaelTegegne/piky-park" target="_blank">
+          <div class="border-2 border-black h-full hover:bg-rose-300" style="border-right:0">
               <p class="text-lg">PikiPark</p>
-              <p>ML model for finding free parking spaces</p>
+              <p class="text-sm">ML model for finding free parking spaces</p>
           </div>
         </a>
         <a href="https://github.com/JamesW6/JamesW6.github.io" target="_blank">
-          <div class="border-2 border-black hover:bg-rose-300">
+          <div class="border-2 border-black h-full hover:bg-rose-300">
             <p class="text-lg">This Website</p>
-            <p>This site is how I dived into web development</p>
+            <p class="text-sm">My dive into web development, deployment, and design</p>
+          </div>
+        </a>
+      </div>
+    </div>
+
+    <div class="border border-black inline-full max-h-px"></div>
+
+    <div id="contact" class="inline-full grid grid-cols-1 grid-rows-1 gap-2">
+      <h1 class="text-2xl">Let's get in touch!</h1>
+      <p class="text-sm">Best way to reach out is email, but I check LinkedIn as well</p>
+      <div class="grid grid-cols-2 py-4">
+        <div class="border border-black" style="border-right:0">
+          <p>jow257 [at] pitt [dot] edu</p>
+        </div>
+        <a href = https://linkedin.com/in/jameswidmer12>
+          <div class="border border-black hover:bg-rose-300"> 
+            <p>linkedin.com/in/jameswidmer12</p>
           </div>
         </a>
       </div>
     </div>
   </div>
 </div>
+
 
